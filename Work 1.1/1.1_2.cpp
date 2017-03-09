bc006646833cd1ad9b476744141363fa33ea5d17
Raquel LOPES DE OLIVEIRA <oliveira.raquel.lopes@gmail.com>
2017-03-09 23:54:12 +0100

    Update process information
    Get name of process and name of the user resposible for it.

--- Work 1.1/1.1_2.cpp
+++ Work 1.1/1.1_2.cpp
@@ -13,7 +13,8 @@ struct Node {
 	string name;
 	long int pid;
 	long int ppid;
-	long int uid;
+	long int user_uid; //User that owns the process
+  string user_name;
 	vector<Node*> children;
 };
 
@@ -55,7 +56,7 @@ Node* build_tree(long tgid) {
   snprintf(path, 40, "/proc/%ld/status", tgid);
 
   statusf = fopen(path, "r");
-  printf("Open file do PID: %li\n", tgid);
+  //printf("Open file do PID: %li\n", tgid);
 
   if(!statusf){
     fprintf(stderr, "Problem trying to open\n");
@@ -63,38 +64,41 @@ Node* build_tree(long tgid) {
   }
 
   tree->pid = tgid;
-  printf("Add PID: %li\n", tgid);
   while(fgets(line, 100, statusf)) {
       if(strncmp(line, "PPid:", 5) == 0){
         tree->ppid = atoi(line + 5);
-        printf("Add PPID: %li of PID: %li\n", tree->ppid, tree->pid);
       }
       if(strncmp(line, "Uid:", 4) == 0){
-        tree->uid = atoi(line + 4);  
-        printf("Add UID: %li of PID: %li\n", tree->uid, tree->pid);
+        //line => Uid :Real, effective, saved set, and  file system UIDs
+        tree->user_uid = atoi(line + 4);  //get real
+        string getUidComplet = run(stc("getent passwd " + to_string(tree->user_uid)));
+        tree->user_name = getUidComplet.substr(0, getUidComplet.find(":"));
+      }
+      if(strncmp(line, "Name:", 5) == 0){
+        tree->name =  string(line).substr(6, strlen(line)); 
       }
       //break;
   }
+  cout << "Add process: " << tgid << "(" << tree->name << ")" << "\nPPid: " << tree->ppid << "\nUid: " << tree->user_uid << "(" << tree->user_name << ")\n\n\n";
   fclose(statusf);
 
-  string listReturn = "pgrep " + to_string(tgid);
-  char * command = new char [listReturn.length()+1];
-  strcpy (command, listReturn.c_str());
-  listReturn = run(command);
+  char * command = stc("pgrep " + to_string(tgid));
+  string listReturn = run(command);
   istringstream f(listReturn);
   string l = "";    
   while (getline(f, l)) {
     int i = stoi(l);
     tree->children.push_back(build_tree(i));
-      printf("Adicionei filho %li do pid %i\n",i , tgid );
+      //printf("Adicionei filho %i do pid %li\n",i , tgid );
   }
-  printf("Adicionei os filho do pid %li\n", tgid );
+  //printf("Adicionei os filho do pid %li\n", tgid );
 
   return tree;   
 }
 
 int main(void){
-  int pid = 6;
+  int pid;;
+  cin >> pid;
   unsigned int microseconds = 10000000; //10 segundos
   Node* tree = build_tree(pid);
 
