1c251f9ef6c734ddf848364fe10c9ab4ce502357
Raquel Oliveira <oliveira.raquel.lopes@gmail.com>
2017-03-10 11:21:53 +0100

    Add process child after try to open
    Before it was creating children by the output of the command 'pgrep -P pid', but it the next recursion it was possible that this child was killed. This way we only add the child if we can open his information in 'proc/child/status'

--- Work 1.1/1.1_2.cpp
+++ Work 1.1/1.1_2.cpp
@@ -48,7 +48,7 @@ char * stc(string a){
   return b;
 }
 
-Node* build_tree(long tgid) {
+Node* build_tree(long tgid, Node * father) {
 	Node* tree = new Node();
   char path[40], line[100], *p;
   FILE* statusf;
@@ -60,47 +60,47 @@ Node* build_tree(long tgid) {
 
   if(!statusf){
     fprintf(stderr, "Problem trying to open\n");
-    exit(-1); 
+   // exit(-1); 
   }
+  else{
+    tree->pid = tgid;
+    while(fgets(line, 100, statusf)) {
+        if(strncmp(line, "PPid:", 5) == 0){
+          tree->ppid = atoi(line + 5);
+          if (father != NULL)
+            father->children.push_back(tree);
+        }
+        if(strncmp(line, "Uid:", 4) == 0){
+          //line => Uid :Real, effective, saved set, and  file system UIDs
+          tree->user_uid = atoi(line + 4);  //get real
+          string getUidComplet = run(stc("getent passwd " + to_string(tree->user_uid)));
+          tree->user_name = getUidComplet.substr(0, getUidComplet.find(":"));
+        }
+        if(strncmp(line, "Name:", 5) == 0){
+          tree->name =  string(line).substr(6, strlen(line)); 
+        }
+        //break;
+    }
+    cout << "Add process: " << tgid << "(" << tree->name << ")" << "\nPPid: " << tree->ppid << "\nUid: " << tree->user_uid << "(" << tree->user_name << ")\n\n\n";
+    fclose(statusf);
 
-  tree->pid = tgid;
-  while(fgets(line, 100, statusf)) {
-      if(strncmp(line, "PPid:", 5) == 0){
-        tree->ppid = atoi(line + 5);
-      }
-      if(strncmp(line, "Uid:", 4) == 0){
-        //line => Uid :Real, effective, saved set, and  file system UIDs
-        tree->user_uid = atoi(line + 4);  //get real
-        string getUidComplet = run(stc("getent passwd " + to_string(tree->user_uid)));
-        tree->user_name = getUidComplet.substr(0, getUidComplet.find(":"));
-      }
-      if(strncmp(line, "Name:", 5) == 0){
-        tree->name =  string(line).substr(6, strlen(line)); 
-      }
-      //break;
+    char * command = stc("pgrep -P" + to_string(tgid));
+    string listReturn = run(command);
+    istringstream f(listReturn);
+    string l = "";    
+    while (getline(f, l)) {
+      int i = stoi(l);
+      build_tree(i, tree);
+    }  
   }
-  cout << "Add process: " << tgid << "(" << tree->name << ")" << "\nPPid: " << tree->ppid << "\nUid: " << tree->user_uid << "(" << tree->user_name << ")\n\n\n";
-  fclose(statusf);
-
-  char * command = stc("pgrep " + to_string(tgid));
-  string listReturn = run(command);
-  istringstream f(listReturn);
-  string l = "";    
-  while (getline(f, l)) {
-    int i = stoi(l);
-    tree->children.push_back(build_tree(i));
-      //printf("Adicionei filho %i do pid %li\n",i , tgid );
-  }
-  //printf("Adicionei os filho do pid %li\n", tgid );
-
-  return tree;   
+  return tree; 
 }
 
 int main(void){
   int pid;;
   cin >> pid;
   unsigned int microseconds = 10000000; //10 segundos
-  Node* tree = build_tree(pid);
+  Node* tree = build_tree(pid, NULL);
 
   char * c_nbProc = stc("ps -A --no-headers | wc -l");
   char * c_nbProcUser = stc("ps hax -o user | sort | uniq -c");
