807988acc10bde8c60b6a54d09bdd6a92f977c5e
Raquel LOPES DE OLIVEIRA <oliveira.raquel.lopes@gmail.com>
2017-06-01 17:17:02 -0300

    Refactor code
    Change multiples vector to a map. Clean code

--- Work 2.2/memoryDisplay.cpp
+++ Work 2.2/memoryDisplay.cpp
@@ -13,53 +13,62 @@
 #include "../Utils/utils.h"
 #include <string.h>
 #include <iomanip> //setw
+#include <map>
 
 using namespace std;
+static string file_name = "infos.txt";
+
+struct Process {
+	long int pid;
+	float mem;
+	int min_flt;
+	int maj_flt;
+};
 
 int main() {
-	vector<int> pid;
-    vector<float> mem;
-    vector<int> min_flt;
-    vector<int> maj_flt;
-    int p, mif, maf; 
-    float m;
+	map<long int, Process> processes;
+  int p, mif, maf;
+  float m;
 	string s = run(stc("ps -eo pid,pmem,min_flt,maj_flt --no-header > infos.txt"));
-  	ifstream myfile ("infos.txt");
-  	if (myfile.is_open()) {
-    	while ( getline (myfile,s) ) {
-      		stringstream ss(s);
-      		ss >> p >> m >> mif >> maf;
-    		pid.push_back(p);
-    		mem.push_back(m);
-    		min_flt.push_back(mif);
-    		maj_flt.push_back(maf);
-    	}
-   		myfile.close();
-  	}
-  	int dummy, cache, swapi, swapo;
+  ifstream myfile (file_name);
+  if (myfile.is_open()) {
+		while ( getline (myfile,s) ) {
+			stringstream ss(s);
+  		ss >> p >> m >> mif >> maf;
+			Process aux;
+			aux.pid = p;
+			aux.mem = m;
+			aux.min_flt = mif;
+			aux.maj_flt = maf;
+			processes[p] = aux;
+    }
+   	myfile.close();
+  }
+  int dummy, cache, swapi, swapo;
  	s = run(stc("vmstat > infos.txt"));
-  myfile.open("infos.txt", ifstream::in);
+  myfile.open(file_name, ifstream::in);
   	if (myfile.is_open()) {
   		getline (myfile,s);
   		getline (myfile,s);
   		getline (myfile,s);
-      	stringstream ss(s);
-      	ss >> dummy; 
-        ss >> dummy;
-        ss >> dummy; 
-        ss >> dummy; 
-        ss >> dummy; 
-        ss >> cache; 
-        ss >> swapi; 
-        ss >> swapo;
+    	stringstream ss(s);
+    	ss >> dummy;
+      ss >> dummy;
+      ss >> dummy;
+      ss >> dummy;
+      ss >> dummy;
+      ss >> cache;
+      ss >> swapi;
+      ss >> swapo;
    		myfile.close();
   	}
   	cout << "Total cache: " << cache << endl;
   	cout << "Swap-in: " << swapi << endl;
   	cout << "Swap-out: " << swapo << endl << endl;
   	cout << "   PID              %MEMORY       MINOR PAGE FAULTS      MAJOR PAGE FAULTS" << endl;
-  	for(int i = 0; i < pid.size(); i++) {
-  		cout << setw(7) << pid[i] << "           " << setw(7) << mem[i] << "            " << setw(7) << min_flt[i] << "            " << setw(7) << maj_flt[i] << endl;
-  	}
+		map<long int,Process>::iterator it = processes.begin();
+		for (it=processes.begin(); it!=processes.end(); ++it)
+		cout << setw(7) << it->first << "           " << setw(7) << it->second.mem << "            " << setw(7) << it->second.min_flt << "            " << setw(7) << it->second.maj_flt << endl;
+
 	return 0;
 }
